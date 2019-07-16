0833cfa8233ff203be9c76e27850ef75abf73515
Alexey <qwazik21@gmail.com>
2019-07-16 10:43:17 +0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,29 @@
 import Vue from "vue";
+import "./plugins/vuetify";
+import { firestorePlugin } from "vuefire";
+import firebase from "firebase/app";
+import "firebase/firestore";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
 
+Vue.use(firestorePlugin);
 Vue.config.productionTip = false;
+
+const fireConfig = {
+  apiKey: "AIzaSyDaU_8YncoS5tnY6HTIahKciIMUlQ5bmF0",
+  authDomain: "tasker-59969.firebaseapp.com",
+  databaseURL: "https://tasker-59969.firebaseio.com",
+  projectId: "tasker-59969",
+  storageBucket: "",
+  messagingSenderId: "865633482633",
+  appId: "1:865633482633:web:f56b3880ca7413c4"
+};
+export const db = firebase.initializeApp(fireConfig).firestore();
+
+const { TimeStamp, GeoPoint } = firebase.firestore;
+export { TimeStamp, GeoPoint };
 
 new Vue({
   router,
